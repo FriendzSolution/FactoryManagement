b258b2a259268a1114ed243d4fa54b1d62abc160
TOUQEER <TOUQEER@DESKTOP-UQDIQPM>
2023-02-20 09:51:01 -0800

    Tauqeer

---
+++ FactoryManagement/FactoryManagement.WebUI/Controllers/SupplierController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FactoryManagement.WebUI.Controllers
+{
+    public class SupplierController : BaseController
+    {
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
